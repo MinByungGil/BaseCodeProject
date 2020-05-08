27077307be35fd3716dedbdc05577d8d9546c6fd
MinByungGil <pegasusx@naver.com>
2020-05-08 10:44:17 +0900

    EaseUtil

---
+++ EaseUtil.cs
@@ -0,0 +1,403 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EaseUtil : MonoBehaviour {
+    
+    public static float EaseValue(iTween.EaseType type, float start, float end, float value)
+    {
+        switch(type)
+        {
+            case iTween.EaseType.easeInQuad:
+                return easeInQuad(start, end, value);
+            case iTween.EaseType.easeOutQuad:
+                return easeInOutQuad(start, end, value);
+            case iTween.EaseType.easeInOutQuad:
+                return easeInOutQuad(start, end, value);
+            case iTween.EaseType.easeInCubic:
+                return easeInCubic(start, end, value);
+            case iTween.EaseType.easeOutCubic:
+                return easeOutCubic(start, end, value);
+            case iTween.EaseType.easeInOutCubic:
+                return easeInOutCubic(start, end, value);
+            case iTween.EaseType.easeInQuart:
+                return easeInQuart(start, end, value);
+            case iTween.EaseType.easeOutQuart:
+                return easeOutQuart(start, end, value);
+            case iTween.EaseType.easeInOutQuart:
+                return easeInOutQuart(start, end, value);
+            case iTween.EaseType.easeInQuint:
+                return easeInQuint(start, end, value);
+            case iTween.EaseType.easeOutQuint:
+                return easeOutQuint(start, end, value);
+            case iTween.EaseType.easeInOutQuint:
+                return easeInOutQuint(start, end, value);
+            case iTween.EaseType.easeInSine:
+                return easeInSine(start, end, value);
+            case iTween.EaseType.easeOutSine:
+                return easeOutSine(start, end, value);
+            case iTween.EaseType.easeInOutSine:
+                return easeInOutSine(start, end, value);
+            case iTween.EaseType.easeInExpo:
+                return easeInExpo(start, end, value);
+            case iTween.EaseType.easeOutExpo:
+                return easeOutExpo(start, end, value);
+            case iTween.EaseType.easeInOutExpo:
+                return easeInOutExpo(start, end, value);
+            case iTween.EaseType.easeInCirc:
+                return easeInCirc(start, end, value);
+            case iTween.EaseType.easeOutCirc:
+                return easeOutCirc(start, end, value);
+            case iTween.EaseType.easeInOutCirc:
+                return easeInOutCirc(start, end, value);
+            case iTween.EaseType.linear:
+                return linear(start, end, value);
+            case iTween.EaseType.spring:
+                return spring(start, end, value);
+            case iTween.EaseType.easeInBounce:
+                return easeInBounce(start, end, value);
+            case iTween.EaseType.easeOutBounce:
+                return easeOutBounce(start, end, value);
+            case iTween.EaseType.easeInOutBounce:
+                return easeInOutBounce(start, end, value);
+            case iTween.EaseType.easeInBack:
+                return easeInBack(start, end, value);
+            case iTween.EaseType.easeOutBack:
+                return easeOutBack(start, end, value);
+            case iTween.EaseType.easeInOutBack:
+                return easeInOutBack(start, end, value);
+            case iTween.EaseType.easeInElastic:
+                return easeInElastic(start, end, value);
+            case iTween.EaseType.easeOutElastic:
+                return easeOutElastic(start, end, value);
+            case iTween.EaseType.easeInOutElastic:
+                return easeInOutElastic(start, end, value);
+        }
+        return float.MinValue;
+    }
+    //iTween.EaseType
+
+    private static float linear(float start, float end, float value)
+    {
+        return Mathf.Lerp(start, end, value);
+    }
+
+    private static float spring(float start, float end, float value)
+    {
+        value = Mathf.Clamp01(value);
+        value = (Mathf.Sin(value * Mathf.PI * (0.2f + 2.5f * value * value * value)) * Mathf.Pow(1f - value, 2.2f) + value) * (1f + (1.2f * (1f - value)));
+        return start + (end - start) * value;
+    }
+    
+    public static float easeInQuad(float start, float end, float value)
+    {
+        end -= start;
+        return end * value * value + start;
+    }
+
+    public static float easeOutQuad(float start, float end, float value)
+    {
+        end -= start;
+        return -end * value * (value - 2) + start;
+    }
+
+    public static float easeInOutQuad(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return end * 0.5f * value * value + start;
+        value--;
+        return -end * 0.5f * (value * (value - 2) - 1) + start;
+    }
+
+    public static float easeInCubic(float start, float end, float value)
+    {
+        end -= start;
+        return end * value * value * value + start;
+    }
+
+    public static float easeOutCubic(float start, float end, float value)
+    {
+        value--;
+        end -= start;
+        return end * (value * value * value + 1) + start;
+    }
+
+    public static float easeInOutCubic(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return end * 0.5f * value * value * value + start;
+        value -= 2;
+        return end * 0.5f * (value * value * value + 2) + start;
+    }
+
+    public static float easeInQuart(float start, float end, float value)
+    {
+        end -= start;
+        return end * value * value * value * value + start;
+    }
+
+    public static float easeOutQuart(float start, float end, float value)
+    {
+        value--;
+        end -= start;
+        return -end * (value * value * value * value - 1) + start;
+    }
+
+    public static float easeInOutQuart(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return end * 0.5f * value * value * value * value + start;
+        value -= 2;
+        return -end * 0.5f * (value * value * value * value - 2) + start;
+    }
+
+    public static float easeInQuint(float start, float end, float value)
+    {
+        end -= start;
+        return end * value * value * value * value * value + start;
+    }
+
+    public static float easeOutQuint(float start, float end, float value)
+    {
+        value--;
+        end -= start;
+        return end * (value * value * value * value * value + 1) + start;
+    }
+
+    public static float easeInOutQuint(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return end * 0.5f * value * value * value * value * value + start;
+        value -= 2;
+        return end * 0.5f * (value * value * value * value * value + 2) + start;
+    }
+
+    public static float easeInSine(float start, float end, float value)
+    {
+        end -= start;
+        return -end * Mathf.Cos(value * (Mathf.PI * 0.5f)) + end + start;
+    }
+
+    public static float easeOutSine(float start, float end, float value)
+    {
+        end -= start;
+        return end * Mathf.Sin(value * (Mathf.PI * 0.5f)) + start;
+    }
+
+    public static float easeInOutSine(float start, float end, float value)
+    {
+        end -= start;
+        return -end * 0.5f * (Mathf.Cos(Mathf.PI * value) - 1) + start;
+    }
+
+    public static float easeInExpo(float start, float end, float value)
+    {
+        end -= start;
+        return end * Mathf.Pow(2, 10 * (value - 1)) + start;
+    }
+
+    public static float easeOutExpo(float start, float end, float value)
+    {
+        end -= start;
+        return end * (-Mathf.Pow(2, -10 * value) + 1) + start;
+    }
+
+    public static float easeInOutExpo(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return end * 0.5f * Mathf.Pow(2, 10 * (value - 1)) + start;
+        value--;
+        return end * 0.5f * (-Mathf.Pow(2, -10 * value) + 2) + start;
+    }
+
+    public static float easeInCirc(float start, float end, float value)
+    {
+        end -= start;
+        return -end * (Mathf.Sqrt(1 - value * value) - 1) + start;
+    }
+
+    public static float easeOutCirc(float start, float end, float value)
+    {
+        value--;
+        end -= start;
+        return end * Mathf.Sqrt(1 - value * value) + start;
+    }
+
+    public static float easeInOutCirc(float start, float end, float value)
+    {
+        value /= .5f;
+        end -= start;
+        if (value < 1) return -end * 0.5f * (Mathf.Sqrt(1 - value * value) - 1) + start;
+        value -= 2;
+        return end * 0.5f * (Mathf.Sqrt(1 - value * value) + 1) + start;
+    }
+
+    /* GFX47 MOD START */
+    public static float easeInBounce(float start, float end, float value)
+    {
+        end -= start;
+        float d = 1f;
+        return end - easeOutBounce(0, end, d - value) + start;
+    }
+    /* GFX47 MOD END */
+
+    /* GFX47 MOD START */
+    //private float bounce(float start, float end, float value){
+    public static float easeOutBounce(float start, float end, float value)
+    {
+        value /= 1f;
+        end -= start;
+        if (value < (1 / 2.75f))
+        {
+            return end * (7.5625f * value * value) + start;
+        }
+        else if (value < (2 / 2.75f))
+        {
+            value -= (1.5f / 2.75f);
+            return end * (7.5625f * (value) * value + .75f) + start;
+        }
+        else if (value < (2.5 / 2.75))
+        {
+            value -= (2.25f / 2.75f);
+            return end * (7.5625f * (value) * value + .9375f) + start;
+        }
+        else
+        {
+            value -= (2.625f / 2.75f);
+            return end * (7.5625f * (value) * value + .984375f) + start;
+        }
+    }
+    /* GFX47 MOD END */
+
+    /* GFX47 MOD START */
+    public static float easeInOutBounce(float start, float end, float value)
+    {
+        end -= start;
+        float d = 1f;
+        if (value < d * 0.5f) return easeInBounce(0, end, value * 2) * 0.5f + start;
+        else return easeOutBounce(0, end, value * 2 - d) * 0.5f + end * 0.5f + start;
+    }
+    /* GFX47 MOD END */
+
+    public static float easeInBack(float start, float end, float value)
+    {
+        end -= start;
+        value /= 1;
+        float s = 1.70158f;
+        return end * (value) * value * ((s + 1) * value - s) + start;
+    }
+
+    public static float easeOutBack(float start, float end, float value)
+    {
+        float s = 1.70158f;
+        end -= start;
+        value = (value) - 1;
+        return end * ((value) * value * ((s + 1) * value + s) + 1) + start;
+    }
+
+    public static float easeInOutBack(float start, float end, float value)
+    {
+        float s = 1.70158f;
+        end -= start;
+        value /= .5f;
+        if ((value) < 1)
+        {
+            s *= (1.525f);
+            return end * 0.5f * (value * value * (((s) + 1) * value - s)) + start;
+        }
+        value -= 2;
+        s *= (1.525f);
+        return end * 0.5f * ((value) * value * (((s) + 1) * value + s) + 2) + start;
+    }
+
+    /* GFX47 MOD START */
+    public static float easeInElastic(float start, float end, float value)
+    {
+        end -= start;
+
+        float d = 1f;
+        float p = d * .3f;
+        float s = 0;
+        float a = 0;
+
+        if (value == 0) return start;
+
+        if ((value /= d) == 1) return start + end;
+
+        if (a == 0f || a < Mathf.Abs(end))
+        {
+            a = end;
+            s = p / 4;
+        }
+        else
+        {
+            s = p / (2 * Mathf.PI) * Mathf.Asin(end / a);
+        }
+
+        return -(a * Mathf.Pow(2, 10 * (value -= 1)) * Mathf.Sin((value * d - s) * (2 * Mathf.PI) / p)) + start;
+    }
+    /* GFX47 MOD END */
+
+    /* GFX47 MOD START */
+    //private float elastic(float start, float end, float value){
+    public static float easeOutElastic(float start, float end, float value)
+    {
+        /* GFX47 MOD END */
+        //Thank you to rafael.marteleto for fixing this as a port over from Pedro's UnityTween
+        end -= start;
+
+        float d = 1f;
+        float p = d * .3f;
+        float s = 0;
+        float a = 0;
+
+        if (value == 0) return start;
+
+        if ((value /= d) == 1) return start + end;
+
+        if (a == 0f || a < Mathf.Abs(end))
+        {
+            a = end;
+            s = p * 0.25f;
+        }
+        else
+        {
+            s = p / (2 * Mathf.PI) * Mathf.Asin(end / a);
+        }
+
+        return (a * Mathf.Pow(2, -10 * value) * Mathf.Sin((value * d - s) * (2 * Mathf.PI) / p) + end + start);
+    }
+
+    /* GFX47 MOD START */
+    public static float easeInOutElastic(float start, float end, float value)
+    {
+        end -= start;
+
+        float d = 1f;
+        float p = d * .3f;
+        float s = 0;
+        float a = 0;
+
+        if (value == 0) return start;
+
+        if ((value /= d * 0.5f) == 2) return start + end;
+
+        if (a == 0f || a < Mathf.Abs(end))
+        {
+            a = end;
+            s = p / 4;
+        }
+        else
+        {
+            s = p / (2 * Mathf.PI) * Mathf.Asin(end / a);
+        }
+
+        if (value < 1) return -0.5f * (a * Mathf.Pow(2, 10 * (value -= 1)) * Mathf.Sin((value * d - s) * (2 * Mathf.PI) / p)) + start;
+        return a * Mathf.Pow(2, -10 * (value -= 1)) * Mathf.Sin((value * d - s) * (2 * Mathf.PI) / p) * 0.5f + end + start;
+    }
+}
