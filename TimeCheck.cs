c1a1675c7ec38c8588f6208810a52fc49de51e62
MinByungGil <pegasusx@naver.com>
2020-05-08 10:44:03 +0900

    TimeCheck Code

---
+++ TimeCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeCheck
+{
+    float _time = 0;
+    public TimeCheck()
+    {
+        _time = Time.realtimeSinceStartup;
+    }
+
+    public void LogTime(string log)
+    {
+#if !REAL
+        Debug.LogFormat("{0} {1}", log, Time.realtimeSinceStartup - _time);
+        _time = Time.realtimeSinceStartup;
+#endif
+    }
+}
