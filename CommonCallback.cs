a6523bc1ddb6e3691ba98e2a3933aee8d7b97711
MinByungGil <pegasusx@naver.com>
2020-10-30 11:50:53 +0900

    코루틴 람다식사용
    CommonCallback

---
+++ CommonCallback.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/*
+ * 예
+ StartCoroutine(CommonCallback.SetTimeout(() => {
+            Debug.Log("A");
+        }, 0.25f));
+
+StartCoroutine(CommonCallback.SetEndOfFrame(() => {
+            Debug.Log("B");
+        }));
+ */
+
+
+public class CommonCallback : MonoBehaviour
+{
+    public static readonly WaitForSeconds WaitOneSeconds = new WaitForSeconds(1f);
+    public static readonly WaitForSeconds WaitHalfSeconds = new WaitForSeconds(0.5f);
+    public static readonly WaitForSeconds WaitQuarterSeconds = new WaitForSeconds(0.25f);
+    public static readonly WaitForSeconds WaitPointOneSeconds = new WaitForSeconds(0.1f);
+    public static readonly WaitForEndOfFrame WaitForEndOfFrame = new WaitForEndOfFrame();
+    public static readonly WaitForFixedUpdate WaitForFixedUpdate = new WaitForFixedUpdate();
+    static public IEnumerator SetTimeout(Action callback, float time)
+    {
+        yield return new WaitForSeconds(time);
+        callback();
+    }
+    static public IEnumerator SetEndOfFrame(Action callback)
+    {
+        yield return WaitForEndOfFrame;
+        callback();
+    }    
+}
